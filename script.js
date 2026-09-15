c74109025f6f77943e17c840fb1c3c9c92ecc872
SMSQLAS <hikarichozai@shikoku-ms.com>
2026-09-15 15:32:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -246,7 +246,10 @@ async function submitResult(event) {
   }
 }
 
-startButton.addEventListener("click", startQuiz);
+startButton.addEventListener("click", () => {
+document.body.classList.add("quiz-started");
+startQuiz();
+});
 nextButton.addEventListener("click", goNext);
 resultForm.addEventListener("submit", submitResult);
 retryButton.addEventListener("click", startQuiz);
